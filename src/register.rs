4bf16ad2e2eabdc9b1673ceecdfacf7cf5c367a8
Rodrigo Valle <rdan.valle@gmail.com>
2020-09-21 01:10:48 -0700

    Skeleton for a basic serial I/O driver

---
+++ src/register.rs
@@ -0,0 +1,51 @@
+use core::ptr;
+
+// pulled these register mappings from the atmega2560 datasheet
+#[allow(dead_code)]
+pub enum RegisterAddr {
+    UDR3   = 0x136,  // USART3 I/O data register
+    UBRR3H = 0x135,  // USART3 baud rate register high byte
+    UBRR3L = 0x134,  // USART3 baud rate register low byte
+    UCSR3C = 0x132,  // USART3 configuration
+    UCSR3B = 0x131,  // USART3 configuration
+    UCSR3A = 0x130,  // USART3 configuration
+
+    UDR2   = 0xD6,  // USART2 I/O data register
+    UBRR2H = 0xD5,  // USART2 baud rate register high byte
+    UBRR2L = 0xD4,  // USART2 baud rate register low byte
+    UCSR2C = 0xD2,  // USART2 configuration
+    UCSR2B = 0xD1,  // USART2 configuration
+    UCSR2A = 0xD0,  // USART2 configuration
+
+    UDR1   = 0xCE,  // USART1 I/O data register
+    UBRR1H = 0xCD,  // USART1 baud rate register high byte
+    UBRR1L = 0xCC,  // USART1 baud rate register low byte
+    UCSR1C = 0xCA,  // USART1 configuration
+    UCSR1B = 0xC9,  // USART1 configuration
+    UCSR1A = 0xC8,  // USART1 configuration
+
+    UDR0   = 0xC6,  // USART0 I/O data register
+    UBRR0H = 0xC5,  // USART0 baud rate register high byte
+    UBRR0L = 0xC4,  // USART0 baud rate register low byte
+    UCSR0C = 0xC2,  // USART0 configuration
+    UCSR0B = 0xC1,  // USART0 configuration
+    UCSR0A = 0xC0,  // USART0 configuration
+}
+
+pub struct Register {
+    addr: u16
+}
+
+impl Register {
+    pub const fn new(addr: RegisterAddr) -> Register {
+        Register { addr: addr as u16 }
+    }
+
+    pub fn read(&self) -> u8 {
+        unsafe { ptr::read_volatile(self.addr as *mut u8) }
+    }
+
+    pub fn write(&self, val: u8) {
+        unsafe { ptr::write_volatile(self.addr as *mut u8, val) }
+    }
+}
---
+++ src/serial.rs
@@ -0,0 +1,101 @@
+// PMS5003 uses these UART settings:
+// - default baud rate: 9600bps
+// - check bit: None
+// - stop bit: 1 bit
+//
+// This is a bare-bones USART driver that supports these requirements. The
+// hardware offers a lot of other features that are unsupported by this driver.
+
+use crate::register::{Register, RegisterAddr};
+
+pub struct Serial {
+    ubrrn: u16
+}
+
+// Frequency of the system clock; the USART on the chip scales
+// this clock down to synchronize with the serial baud rate.
+// The Arduino Mega 2650 board ships with an external 16MHz system clock.
+const CLK: u32 = 16_000_000;
+
+impl Serial {
+    pub const fn new(baud_rate: u32) -> Serial {
+        // this formula was taken from the atmega2650 datasheet
+        let ubrrn = (CLK / (16 * baud_rate) - 1) as u16;
+        Serial { ubrrn }
+    }
+
+    // to intialize the UART we need to
+    // - set the baud rate
+    // - set the frame format
+    // - enable Tx/Rx
+    //
+    // Registers (roughly):
+    // - UBRRnH: four most significant bits of baud rate
+    // - UBRRnL: 8 least significant bits of baud rate
+    // - UCSRB: enable/disable Tx/Rx
+    // - UCSRC: specify frame format
+    //
+    // Flags
+    // - TXCn flag: check if transmitter has completed all transfers
+    // - RXC flag: check if no unread data in the receive buffer
+    pub fn init(&self) {
+        // TODO:
+        // datasheet also recommends zeroing the power reduction bit PRUSART{N}
+        // this might have been set if we disabled the USART previously to save
+        // power.
+
+        // set the baud rate
+        let ubrrh = Register::new(RegisterAddr::UBRR0H);
+        let ubrrl = Register::new(RegisterAddr::UBRR0L);
+        ubrrh.write((self.ubrrn >> 8) as u8);
+        ubrrl.write(self.ubrrn as u8);
+
+        // USART status and control registers
+        let ucsrb = Register::new(RegisterAddr::UCSR0B);
+        let ucsrc = Register::new(RegisterAddr::UCSR0C);
+
+        // flags to control rx/tx on the USART
+        let rx_enable = 0b00010000;
+        let tx_enable = 0b00001000;
+
+        // These flags tell the hardware to send interrupts for async handling
+        // of serial I/O. Could be interesting but completely unecessary for now
+        // let rx_complete_interrupt_enable = 0b10000000;
+        // let tx_complete_interrupt_enable = 0b01000000;
+
+        // aka uscz (usart character size)
+        // there are only 3 bits to represent this quantity
+        let data_bits = 8;
+        let data_bits = data_bits & 0b00000111;
+
+        // aka upm (usart parity mode)
+        // let parity_bit = 0b00100000;      // enable partiy check
+        // let parity_bit_odd = 0b00010000;  // even or odd parity check
+
+        // aka usbs (usart stop bit select)
+        // let stop_bits = 1;  // \in {1, 2}
+
+        // get the most significant data bit to put in ucsrb
+        let data_bit_h = data_bits >> 2;
+        // get the two least significant data bits to put in ucsrc
+        let data_bit_l = data_bits & 0b00000011;
+
+        ucsrb.write(rx_enable | tx_enable | (data_bit_h << 1));
+        ucsrc.write(data_bit_l << 1);
+    }
+
+    // init() must be called before tx()
+    // TODO: express this with the type system so not calling init() is a
+    // compile time error
+    pub fn tx(&self, ) {
+    }
+
+    // init() must be called before rx()
+    // TODO: express this with the type system so not calling init() is a
+    // compile error
+    pub fn rx(&self) {
+    }
+
+    // TODO: add some helpers to read RX/TX complete bits so we can busy-wait on
+    // them until we're ready to read data out. I think these are in ucsra.
+}
